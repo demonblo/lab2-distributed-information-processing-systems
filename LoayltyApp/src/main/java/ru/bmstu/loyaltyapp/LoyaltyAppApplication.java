95fda2757031bfd604a195fb651f13e4d9b48c17
Dmitry <demonblo007@gmail.com>
2024-01-19 08:39:35 +0300

    help someone

--- LoayltyApp/src/main/java/ru/bmstu/loyaltyapp/LoyaltyAppApplication.java
+++ LoayltyApp/src/main/java/ru/bmstu/loyaltyapp/LoyaltyAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoayltyAppApplication {
+public class LoyaltyAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LoayltyAppApplication.class, args);
+        SpringApplication.run(LoyaltyAppApplication.class, args);
     }
 
 }
